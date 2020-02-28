a571161a53ecf3c9a7f87b9bc0437c51d33d269d
vibecode <rumashkin@gmail.com>
2020-02-28 15:47:37 +0100

    setup routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { HomePageComponent } from './home-page/home-page.component'
+import { PostPageComponent } from './post-page/post-page.component'
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component'
+import { AdminModule } from './admin/admin.module'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    PostPageComponent,
+    MainLayoutComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
