a419dbd430750808feec9686fd3c5084148ab25d
vibecode <rumashkin@gmail.com>
2020-03-06 20:14:18 +0100

    add post editor

--- src/app/admin/create-page/create-page.component.ts
+++ src/app/admin/create-page/create-page.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { Post } from '../shared/interfaces'
 
 @Component({
   selector: 'app-create-page',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-page.component.scss']
 })
 export class CreatePageComponent implements OnInit {
+  form: FormGroup
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl(null, Validators.required),
+      text: new FormControl(null, Validators.required),
+      author: new FormControl(null, Validators.required)
+    })
   }
 
+  submit() {
+    if (this.form.invalid) {
+      return
+    }
+    const { title, author, text } = this.form.value
+
+    const post: Post = {
+      title,
+      author,
+      text,
+      date: new Date()
+    }
+  }
 }
