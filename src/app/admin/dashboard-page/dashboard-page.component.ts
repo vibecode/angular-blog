c4d42d9f16687b03f664082513b3efeadb3f9097
vibecode <rumashkin@gmail.com>
2020-03-11 15:19:57 +0100

    implement posts list

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core'
+import { PostService } from '../shared/services/post.service'
+import { Post } from '../shared/interfaces'
+import { Subscription } from 'rxjs'
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
+  posts: Post[] = []
+  pSub: Subscription
 
-  constructor() { }
+  constructor(private postsService: PostService) {}
 
   ngOnInit(): void {
+    this.pSub = this.postsService.getAll().subscribe(posts => {
+      this.posts = posts
+    })
   }
 
+  ngOnDestroy(): void {
+    if (this.pSub) {
+      this.pSub.unsubscribe()
+    }
+  }
+
+  remove(id: string) {}
 }
